417f7509afc5099d0eab2ffefe10814cb46dcbad
sim511777 <sim51177@gmail.com>
2022-10-18 14:23:44 +0900

    System.Random, CRT rand(), 구현

--- RandomImage/Form1.Designer.cs
+++ RandomImage/Form1.Designer.cs
@@ -23,13 +23,166 @@
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbxRandomAlgorithm = new System.Windows.Forms.ListBox();
+            this.numHeight = new System.Windows.Forms.NumericUpDown();
+            this.numWidth = new System.Windows.Forms.NumericUpDown();
+            this.btnAlloc = new System.Windows.Forms.Button();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.ibxDraw = new ShimLib.ImageBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbxRandomAlgorithm);
+            this.panel1.Controls.Add(this.numHeight);
+            this.panel1.Controls.Add(this.numWidth);
+            this.panel1.Controls.Add(this.btnAlloc);
+            this.panel1.Controls.Add(this.btnRandom);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(1298, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(233, 879);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbxRandomAlgorithm
+            // 
+            this.lbxRandomAlgorithm.FormattingEnabled = true;
+            this.lbxRandomAlgorithm.ItemHeight = 12;
+            this.lbxRandomAlgorithm.Items.AddRange(new object[] {
+            "System.Random",
+            "CRT rnad()"});
+            this.lbxRandomAlgorithm.Location = new System.Drawing.Point(6, 95);
+            this.lbxRandomAlgorithm.Name = "lbxRandomAlgorithm";
+            this.lbxRandomAlgorithm.Size = new System.Drawing.Size(120, 76);
+            this.lbxRandomAlgorithm.TabIndex = 4;
+            // 
+            // numHeight
+            // 
+            this.numHeight.Location = new System.Drawing.Point(6, 39);
+            this.numHeight.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numHeight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numHeight.Name = "numHeight";
+            this.numHeight.Size = new System.Drawing.Size(120, 21);
+            this.numHeight.TabIndex = 3;
+            this.numHeight.Value = new decimal(new int[] {
+            8192,
+            0,
+            0,
+            0});
+            // 
+            // numWidth
+            // 
+            this.numWidth.Location = new System.Drawing.Point(6, 12);
+            this.numWidth.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numWidth.Name = "numWidth";
+            this.numWidth.Size = new System.Drawing.Size(120, 21);
+            this.numWidth.TabIndex = 2;
+            this.numWidth.Value = new decimal(new int[] {
+            8192,
+            0,
+            0,
+            0});
+            // 
+            // btnAlloc
+            // 
+            this.btnAlloc.Location = new System.Drawing.Point(6, 66);
+            this.btnAlloc.Name = "btnAlloc";
+            this.btnAlloc.Size = new System.Drawing.Size(75, 23);
+            this.btnAlloc.TabIndex = 1;
+            this.btnAlloc.Text = "Alloc";
+            this.btnAlloc.UseVisualStyleBackColor = true;
+            this.btnAlloc.Click += new System.EventHandler(this.btnAlloc_Click);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(6, 177);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // ibxDraw
+            // 
+            this.ibxDraw.BackColor = System.Drawing.Color.Gray;
+            this.ibxDraw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibxDraw.Location = new System.Drawing.Point(0, 0);
+            this.ibxDraw.Name = "ibxDraw";
+            this.ibxDraw.Option.CenterLineColor = System.Drawing.Color.Yellow;
+            this.ibxDraw.Option.ColorMap = ShimLib.EColorMap.Turbo;
+            this.ibxDraw.Option.CustomValueMax = 255D;
+            this.ibxDraw.Option.CustomValueMin = 0D;
+            this.ibxDraw.Option.FloatDigit = 3;
+            this.ibxDraw.Option.InfoEFont = ShimLib.EFont.unifont_13_0_06_bdf;
+            this.ibxDraw.Option.MaxZoomLevel = 16;
+            this.ibxDraw.Option.MinZoomLevel = -16;
+            this.ibxDraw.Option.PanClampMargin = 20;
+            this.ibxDraw.Option.RoiRectangleColor = System.Drawing.Color.Blue;
+            this.ibxDraw.Option.TimeCheckCount = 100;
+            this.ibxDraw.Option.UseColorMap = false;
+            this.ibxDraw.Option.UseCustomValueRange = false;
+            this.ibxDraw.Option.UseDrawCenterLine = true;
+            this.ibxDraw.Option.UseDrawCursorInfo = true;
+            this.ibxDraw.Option.UseDrawDebugInfo = true;
+            this.ibxDraw.Option.UseDrawPixelValue = true;
+            this.ibxDraw.Option.UseDrawRoiRectangles = true;
+            this.ibxDraw.Option.UsePalette = false;
+            this.ibxDraw.Option.UsePanClamp = true;
+            this.ibxDraw.Option.UseParallelToDraw = false;
+            this.ibxDraw.Option.UseShowText = false;
+            this.ibxDraw.Size = new System.Drawing.Size(1298, 879);
+            this.ibxDraw.SzPan = new System.Drawing.Size(0, 0);
+            this.ibxDraw.TabIndex = 1;
+            this.ibxDraw.Text = "imageBox1";
+            this.ibxDraw.ZoomLevel = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1531, 879);
+            this.Controls.Add(this.ibxDraw);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private ShimLib.ImageBox ibxDraw;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.NumericUpDown numHeight;
+        private System.Windows.Forms.NumericUpDown numWidth;
+        private System.Windows.Forms.Button btnAlloc;
+        private System.Windows.Forms.ListBox lbxRandomAlgorithm;
     }
 }
 
--- RandomImage/Form1.cs
+++ RandomImage/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using ShimLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +15,113 @@ namespace RandomImage {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+            lbxRandomAlgorithm.SelectedIndex = 0;
         }
+        [DllImport("msvcrt")] public static extern int rand();
+
+        ImageBuffer imgBuf;
+
+        private unsafe void RandomImage_CRTrand() {
+            for (int y = 0; y < imgBuf.Height; y++) {
+                var ptr = (byte*)imgBuf.Buffer + imgBuf.Step * y;
+                for (int x = 0; x < imgBuf.Width; x++, ptr++) {
+                    int gray = rand() % 256;
+                    *ptr = *(byte*)&gray;
+                }
+            }
+        }
+
+        private unsafe void RandomImage_SystemRandom() {
+            Random rnd = new Random();
+            for (int y = 0; y < imgBuf.Height; y++) {
+                var ptr = (byte*)imgBuf.Buffer + imgBuf.Step * y;
+                for (int x = 0; x < imgBuf.Width; x++, ptr++) {
+                    int gray = rnd.Next(256);
+                    *ptr = *(byte*)&gray;
+                }
+            }
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e) {
+            switch (lbxRandomAlgorithm.SelectedIndex) {
+                case 0:
+                    RandomImage_SystemRandom();
+                    break;
+                case 1:
+                    RandomImage_CRTrand();
+                    break;
+            }
+            ibxDraw.Redraw();
+        }
+
+        private void btnAlloc_Click(object sender, EventArgs e) {
+            if (imgBuf != null)
+                imgBuf.Dispose();
+            int width = (int)numWidth.Value;
+            int height = (int)numHeight.Value;
+            imgBuf = new ImageBuffer(width, height, ImageBufferFormat.Gray8);
+            ibxDraw.SetImageBuffer(imgBuf.Buffer, imgBuf.Width, imgBuf.Height, 8, imgBuf.Step, imgBuf.BufferFormat, null);
+            ibxDraw.Redraw();
+        }
+    }
+
+    public class ImageBuffer : IDisposable {
+        public int Width { get; } = 0;
+        public int Height { get; } = 0;
+        public int Step { get; } = 0;
+        public ImageBufferFormat BufferFormat { get; } = ImageBufferFormat.Undefined;
+        public IntPtr Buffer { get; } = IntPtr.Zero;
+        public bool External { get; } = false;
+
+        #region IDisposable Support
+        private bool disposedValue = false; // 중복 호출을 검색하려면
+
+        public ImageBuffer(int width, int height, int step, IntPtr buffer, ImageBufferFormat bufferFormat) {
+            Width = width;
+            Height = height;
+            Step = step;
+            BufferFormat = bufferFormat;
+            Buffer = buffer;
+            External = true;
+        }
+
+        public ImageBuffer(int width, int height, ImageBufferFormat bufferFormat) {
+            Width = width;
+            Height = height;
+            Step = Width;
+            BufferFormat = bufferFormat;
+            Buffer = Marshal.AllocHGlobal(Step * Height);
+            External = false;
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                    // TODO: 관리되는 상태(관리되는 개체)를 삭제합니다.
+                }
+
+                // TODO: 관리되지 않는 리소스(관리되지 않는 개체)를 해제하고 아래의 종료자를 재정의합니다.
+                if (External)
+                    Marshal.FreeHGlobal(Buffer);
+                // TODO: 큰 필드를 null로 설정합니다.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 위의 Dispose(bool disposing)에 관리되지 않는 리소스를 해제하는 코드가 포함되어 있는 경우에만 종료자를 재정의합니다.
+        // ~ImageBuffer() {
+        //   // 이 코드를 변경하지 마세요. 위의 Dispose(bool disposing)에 정리 코드를 입력하세요.
+        //   Dispose(false);
+        // }
+
+        // 삭제 가능한 패턴을 올바르게 구현하기 위해 추가된 코드입니다.
+        public void Dispose() {
+            // 이 코드를 변경하지 마세요. 위의 Dispose(bool disposing)에 정리 코드를 입력하세요.
+            Dispose(true);
+            // TODO: 위의 종료자가 재정의된 경우 다음 코드 줄의 주석 처리를 제거합니다.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
